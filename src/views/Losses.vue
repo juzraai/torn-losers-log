fd0c9ac4cf1dfe768e72c2ee9c77da413e7c34f6
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-03-07 11:25:52 +0100

    feat: added pagination to losses list

--- src/views/Losses.vue
+++ src/views/Losses.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<ul class="list-group">
+		<ul class="list-group mb-3">
 			<li
 				class="list-group-item"
 				:key="a.code"
@@ -20,9 +20,70 @@
 				</a>
 			</li>
 		</ul>
-		<div>
-			left: items per page settings
-			right: prev/next btns
+		<div class="d-flex justify-content-between">
+			<ul class="pagination pagination-sm">
+				<li class="page-item"><a
+						class="page-link"
+						href="#"
+					>10</a></li>
+				<li class="page-item"><a
+						class="page-link"
+						href="#"
+					>20</a></li>
+				<li class="page-item"><a
+						class="page-link"
+						href="#"
+					>25</a></li>
+				<li class="page-item"><a
+						class="page-link"
+						href="#"
+					>50</a></li>
+			</ul>
+			<ul class="pagination pagination-sm">
+				<li
+					class="page-item"
+					:class="{ disabled: page <= 0 }"
+					@click="page = 0"
+				>
+					<a
+						class="page-link"
+						href="#"
+					>First</a>
+				</li>
+				<li
+					class="page-item"
+					:class="{ disabled: page <= 0 }"
+					@click="page--"
+				>
+					<a
+						class="page-link"
+						href="#"
+					>Previous</a>
+				</li>
+				<li class="disabled page-item">
+					<span class="page-link">{{ page + 1 }} / {{ pageCount }}</span>
+				</li>
+				<li
+					class="page-item"
+					:class="{ disabled: page >= pageCount - 1 }"
+					@click="page++"
+				>
+					<a
+						class="page-link"
+						href="#"
+					>Next</a>
+				</li>
+				<li
+					class="page-item"
+					:class="{ disabled: page >= pageCount - 1 }"
+					@click="page = pageCount - 1"
+				>
+					<a
+						class="page-link"
+						href="#"
+					>Last</a>
+				</li>
+			</ul>
 		</div>
 	</div>
 </template>
@@ -35,13 +96,20 @@ export default {
 	components: { Player },
 	data() {
 		return {
+			limit: 10,
 			page: 0,
 		};
 	},
 	computed: {
 		...mapState(["losses"]),
+		offset() {
+			return this.limit * this.page;
+		},
+		pageCount() {
+			return Math.ceil(this.losses.length / this.limit);
+		},
 		lossesOnPage() {
-			return this.losses.slice(this.page * 10, this.page * 10 + 10);
+			return this.losses.slice(this.offset, this.offset + this.limit);
 		},
 	},
 };
