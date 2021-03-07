af6c6332c7daecbbf4fa25475b30759efc713149
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-03-07 22:17:45 +0100

    feat: added full readable timestamps

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,19 +1,23 @@
 <template>
 	<div
-		class="align-items-center d-flex list-group-item py-2"
+		class="align-items-center d-flex list-group-item px-2 py-1"
 		:class="{ 'bg-light': a.paid, 'text-muted': a.paid }"
 	>
+		<small class="mr-2 text-muted">
+			{{ formatTimestamp(a.timestamp_ended) }}
+			<span v-if="a.timestamp_started">
+				<br>
+				{{ formatTimestamp(a.timestamp_started) }}
+			</span>
+		</small>
 		<div>
+
 			You lost
 			<strong v-if="a.attacks">{{ a.attacks.length }}x</strong>
 			to
 			<Player
 				:id="a.defender_id"
 				:variant="a.paid ? 'muted' :'dark'"
-			/>
-			<Timeago
-				:auto-update="60"
-				:datetime="a.timestamp_ended * 1000"
 			/>
 			<span
 				class="ml-2 text-success"
@@ -23,7 +27,7 @@
 				<strong>Paid</strong>
 			</span>
 		</div>
-		<div class="ml-auto">
+		<div class="ml-auto my-1">
 			<!--<button class="btn btn-sm btn-outline-info mx-1">Proof</button>-->
 			<button
 				class="btn btn-sm btn-outline-success mx-1"
@@ -40,6 +44,7 @@
 </template>
 
 <script>
+import moment from 'moment';
 import { mapActions } from "vuex";
 import Player from "@/components/Player.vue";
 
@@ -48,6 +53,9 @@ export default {
 	props: ["a"],
 	methods: {
 		...mapActions(["markAsPaid", "markAsUnpaid"]),
+		formatTimestamp(t) {
+			return moment(t * 1000).utcOffset(0).format('HH:mm:ss DD/MM/YY')
+		}
 	},
 };
 </script>
