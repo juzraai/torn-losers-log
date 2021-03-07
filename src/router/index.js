6092b5e3575bdc81599e47fb51ddb78d10bf96d7
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-03-07 22:43:50 +0100

    defined some route tooltips

--- src/router/index.js
+++ src/router/index.js
@@ -3,20 +3,21 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-function r(path, name, icon) {
+function r(path, name, icon, tooltip) {
 	return {
 		path,
 		name,
 		icon,
+		tooltip,
 		component: () => import(`../views/${name}.vue`)
 	}
 }
 
 export const routes = [
-	r('/', 'Losses', 'fa-list'),
-	r('/sessions', 'Sessions', 'fa-layer-group'),
-	r('/clients', 'Clients', 'fa-user'),
-	r('/stats', 'Stats', 'fa-chart-bar'),
+	r('/', 'Losses', 'fa-list', 'List of individual losses, just like on TORN or YATA.'),
+	r('/sessions', 'Sessions', 'fa-layer-group', 'Groups of consecutive losses to the same client with the same paid status.'),
+	r('/clients', 'Clients', 'fa-user', 'Losses aggregated by client.'),
+	//r('/stats', 'Stats', 'fa-chart-bar'),
 ]
 
 const router = new VueRouter({
