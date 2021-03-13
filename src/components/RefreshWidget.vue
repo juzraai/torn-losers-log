41c635a7700dddf74805c7857d8e8e802d2ed54a
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-03-13 11:43:48 +0100

    reworking UI - building refresh widget

--- src/components/RefreshWidget.vue
+++ src/components/RefreshWidget.vue
@@ -1,14 +1,62 @@
 <template>
-	<Widget card-classes="bg-danger text-white">
-		kismaci
+	<Widget
+		card-classes="alert alert-danger p-0"
+		role="button"
+		@click="fetchLosses"
+	>
+		<h6 class="card-title">Log refreshed</h6>
+		<h5 class="card-subtitle">
+			<timeago
+				:auto-update="60"
+				:datetime="lastUpdate"
+				v-if="lastUpdate"
+			/>
+		</h5>
+		<div class="align-items-center d-flex flex-grow-1 justify-content-center">
+			<i class="alert-link fas fa-sync-alt mt-3"></i>
+		</div>
 	</Widget>
 </template>
 
 <script>
+import { mapActions, mapState } from "vuex";
 import Widget from "@/components/Widget.vue";
 
 export default {
 	components: { Widget },
+	computed: {
+		...mapState(["lastUpdate"]),
+	},
+	methods: {
+		...mapActions(["fetchLosses"]),
+	},
 };
 </script>
+
+<style lang="scss" scoped>
+i {
+	font-size: 2rem;
+	transition: font-size 0.2s;
+}
+.alert:hover {
+	background: blue !important;
+
+	i {
+		font-size: 2.5rem;
+		//-webkit-animation: fa-spin 2s linear infinite;
+		//animation: fa-spin 2s linear infinite;
+	}
+}
+
+@keyframes fa-spin {
+	0% {
+		-webkit-transform: rotate(0deg);
+		transform: rotate(0deg);
+	}
+	100% {
+		-webkit-transform: rotate(1turn);
+		transform: rotate(1turn);
+	}
+}
+</style>
 
--- src/components/Widget.vue
+++ src/components/Widget.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="d-flex flex-column h-100">
 		<div class="card flex-grow-1 mb-4 shadow-sm" :class="cardClasses">
-			<div class="card-body p-3">
+			<div class="card-body d-flex flex-column p-3">
 				<slot />
 			</div>
 		</div>
