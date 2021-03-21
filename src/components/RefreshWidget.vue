e2f92f69bf731f68ed678bd628ec20bd32056d98
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-03-21 09:56:06 +0100

    reworked refresh widget to show some db info

--- src/components/RefreshWidget.vue
+++ src/components/RefreshWidget.vue
@@ -1,22 +1,29 @@
 <template>
-	<Widget card-class="alert-info" title="Log refreshed">
-		<h5 class="mb-3">
-			<timeago
-				:auto-update="60"
-				:datetime="lastUpdate"
-				v-if="lastUpdate"
-			/>
-		</h5>
-		<div class="align-items-end d-flex flex-grow-1">
+	<Widget card-class="alert-info">
+		<h6 class="card-title">
+			<strong>{{ losses.length }}</strong>
+			losses between
+		</h6>
+		<p class="flex-grow-1">
+			{{ formatTimestamp(losses[losses.length - 1].timestamp_ended) }}
+			<br>
+			{{ formatTimestamp(losses[0].timestamp_ended) }}
+		</p>
+		<div class="align-items-center d-flex">
 			<button
-				class="btn btn-lg btn-info"
+				class="btn btn-info mr-3"
 				title="1 API call"
 				v-b-tooltip.hover.bottom
 				@click="fetchLosses"
 			>
-				<i class="fas fa-sync-alt mr-2"></i>
-				Refresh
+				<i class="fas fa-sync-alt"></i>
 			</button>
+			<timeago
+				:auto-update="60"
+				class="small"
+				:datetime="lastUpdate"
+				v-if="lastUpdate"
+			/>
 		</div>
 	</Widget>
 </template>
@@ -24,14 +31,16 @@
 <script>
 import { mapActions, mapState } from "vuex";
 import Widget from "@/components/Widget.vue";
+import { formatTimestamp } from "@/util.js";
 
 export default {
 	components: { Widget },
 	computed: {
-		...mapState(["lastUpdate"]),
+		...mapState(["lastUpdate", "losses"]),
 	},
 	methods: {
 		...mapActions(["fetchLosses"]),
+		formatTimestamp,
 	},
 };
 </script>
