bb1a7ee152d7afb11e9dad86ca779b721605e2db
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-03-28 15:30:53 +0200

    reworked footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,39 +1,37 @@
 <template>
 	<div class="bg-dark text-white">
 		<div class="container my-3">
-			<p class="text-center">
-			Version {{ version }}
-
-			<span class="d-none d-md-inline mx-3">&middot;</span>
-			<br class="d-md-none">
-
-			Released
-			<timeago
-				:auto-update="60"
-				:datetime="buildTime"
-				v-if="buildTime"
-			/>
-
-			<span class="d-none d-md-inline mx-3">&middot;</span>
-			<br class="d-md-none">
-
-			Source code, license:
-			<a
-				class="font-weight-bold text-white"
-				href="https://github.com/juzraai/torn-losers-log"
-				target="_blank"
-			>GitHub</a>
-
-			<span class="d-none d-md-inline mx-3">&middot;</span>
-			<br class="d-md-none">
-
-			Issues, donations:
-			<a
-				class="font-weight-bold text-white"
-				href="https://www.torn.com/profiles.php?XID=2413874"
-				target="_blank"
-			>[2413874]</a>
-			</p>
+			<div class="row">
+				<div class="col-md-4 mb-4 mb-md-0">
+					Version <strong>{{ version }}</strong>
+					<br>
+					Released
+					<timeago
+						:auto-update="60"
+						class="font-weight-bold"
+						:datetime="buildTime"
+						v-if="buildTime"
+					/>
+				</div>
+				<div class="col">
+					Source code, license:
+					<br>
+					<a
+						class="font-weight-bold text-white"
+						href="https://github.com/juzraai/torn-losers-log"
+						target="_blank"
+					>GitHub</a>
+				</div>
+				<div class="col">
+					Issues, donations:
+					<br>
+					<a
+						class="font-weight-bold text-white"
+						href="https://www.torn.com/profiles.php?XID=2413874"
+						target="_blank"
+					>[2413874]</a>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
