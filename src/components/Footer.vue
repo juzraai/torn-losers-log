253e03ab9eb6854d0f39fccddbe8c1d3cb7345bb
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-03-28 15:16:20 +0200

    added build time to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,14 +2,22 @@
 	<div class="bg-dark text-white">
 		<div class="container my-3">
 			<p class="text-center">
+				Released
+				<timeago
+					:auto-update="60"
+					:datetime="buildTime"
+					v-if="buildTime"
+				/>
+				<span class="d-none d-md-inline mx-3">&middot;</span>
+				<br class="d-md-none">
 				Source code, license:
 				<a
 					class="font-weight-bold text-white"
 					href="https://github.com/juzraai/torn-losers-log"
 					target="_blank"
 				>GitHub</a>
-				<span class="d-none d-sm-inline mx-3">&middot;</span>
-				<br class="d-sm-none">
+				<span class="d-none d-md-inline mx-3">&middot;</span>
+				<br class="d-md-none">
 				Issues, donations:
 				<a
 					class="font-weight-bold text-white"
@@ -20,6 +28,23 @@
 		</div>
 	</div>
 </template>
+
+<script>
+export default {
+	data() {
+		return {
+			buildTime: null,
+		};
+	},
+	mounted() {
+		const t =
+			document
+				.getElementsByTagName("html")[0]
+				.getAttribute("data-build-timestamp") || 0;
+		this.buildTime = Number(t);
+	},
+};
+</script>
 
 <style scoped>
 dt {
