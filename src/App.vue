9413a37ecf5881dc2ea9458787e292a84aecc8b5
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-04-16 22:16:43 +0200

    added all ideas to TODOs

--- src/App.vue
+++ src/App.vue
@@ -14,13 +14,19 @@
 </template>
 
 <script>
+// TODO under MD, make tabs flex-grow:1
+// TODO invoice generator in  modal/separate page. list of unpaid clients with editable price and remove from invoice btn, calculates subtotals and total, formats as PRE, or maybe provides more format options
 // TODO upgrade to Vue CLI 4, Vue 3
 // TODO app logo, brand, favicon
-// TODO bar chart options for day limit: 10, 15, 21, 30
+// TODO promo poster to forum topic (366 or 602px width) - highlight main features, zoom circles
+// TODO settings page / widget menu - bar chart options for day limit: 10, 15, 21, 30
+// TODO settings page / widget menu - select log tabs to show
+// TODO Hide feature A) "Hide" button in log entry: exclude losses from top widgets and Clients/Unpaid tabs. on other tabs it can be with lighter font color, without Paid button but with Unhide. grouping should consider "hidden" flag too.
+// TODO Hide feature B) "Hide" button on client level: hides that client from top widgets and all tabs. Can be unhidden on settings page.
 // TODO mark amount of paid losses in bar chart by stacking bars?
-// TODO "Hide" button: exclude losses from top widgets and Clients/Unpaid tabs. on other tabs it can be with lighter font color, without Paid button but with Unhide. grouping should consider "hidden" flag too
 // TODO display about markdown file in app
-// TODO invoice generator: modal window displays unpaid clients with editable price and remove from invoice, calculates subtotals and total, formats as PRE
+// TODO dark mode?
+// TODO match TORN colors?
 
 import Footer from "@/components/Footer.vue";
 import Navbar from "@/components/Navbar.vue";
