cf0fa0551b1492f9b61ceca3bbf1861e35eab9af
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-12-13 00:02:00 +0100

    added footer layout and content

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,13 +1,45 @@
 <template>
-	<div>
-		<p>
-			v{{ $config.APP_VERSION }}
-			(released
-			<Timeago
-				:auto-update="60"
-				:datetime="$config.BUILD_TIMESTAMP"
-			/>)
-		</p>
+	<div class="bg-dark py-2 text-light">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<p>
+						<strong>TORN City Losers' Log (TLL)</strong>
+						<br>
+						<strong>v{{ $config.APP_VERSION }}</strong>
+						(released
+						<Timeago
+							:auto-update="60"
+							:datetime="$config.BUILD_TIMESTAMP"
+						/>)
+					</p>
+					<p>
+						<i class="fab fa-github fa-fw mr-1" />
+						<a
+							href="https://github.com/juzraai/torn-losers-log"
+							target="_blank"
+						>Source code on GitHub</a>
+					</p>
+				</div>
+				<div class="col">
+					<p>
+						<strong>
+							Created by <a
+								href="https://www.torn.com/profiles.php?XID=2413874"
+								target="_blank"
+							>[2413874]</a>,
+						</strong>
+						please feel free to contact me in game if you have any questions, issues, ideas or feature requests.
+					</p>
+					<p>
+						In-game donations and <a
+							href="https://www.torn.com/forums.php#/p=threads&f=67&t=16219540"
+							target="_blank"
+						>forum thread</a> upvotes are much appreciated.
+					</p>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -19,3 +51,10 @@ export default {
 	}),
 };
 </script>
+
+<style scoped>
+a {
+	color: white;
+	font-weight: bold;
+}
+</style>
