51bcc124fa65124c54c5ed4b0173fd4b3ef2ffd4
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-12-14 17:07:40 +0100

    removed vuex cookie persistence completely

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,6 @@ export default {
 	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
 	plugins: [
 		'@/plugins/timeago.js',
-		'@/plugins/persistence.js',
 	],
 
 	// Auto import components: https://go.nuxtjs.dev/config-components
@@ -56,7 +55,6 @@ export default {
 		'bootstrap-vue/nuxt',
 		// https://go.nuxtjs.dev/axios
 		'@nuxtjs/axios',
-		'cookie-universal-nuxt',
 	],
 
 	publicRuntimeConfig: {
