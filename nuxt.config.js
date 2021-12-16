b762c211a888b39ffb6e6382faab87a8aa1488a3
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-12-16 11:02:32 +0100

    tidy up nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,74 +5,9 @@ const changelog = fs.readFileSync('./CHANGELOG.md', 'utf8');
 const { version } = JSON.parse(packageJson);
 
 export default {
-	// Target: https://go.nuxtjs.dev/config-target
-	target: 'static',
-
-	// Global page headers: https://go.nuxtjs.dev/config-head
-	head: {
-		title: 'TLL',
-		titleTemplate: '%s | TLL',
-		htmlAttrs: {
-			lang: 'en'
-		},
-		meta: [
-			{ charset: 'utf-8' },
-			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: 'TLL is a tool which aims to help TORN City\'s loss sellers by providing them automatic counting, performance statistics and a proof system.' }
-		],
-		link: [
-			{
-				rel: 'stylesheet',
-				href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css'
-			},
-			{
-				rel: 'stylesheet',
-				href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/fontawesome.min.css'
-			}
-		],
-		// TODO favicon, fonts
-	},
-
-	// Global CSS: https://go.nuxtjs.dev/config-css
-	css: ['@/assets/style.scss'],
-
-	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-	plugins: [
-		'@/plugins/database.client.js',
-		'@/plugins/storage.client.js',
-		'@/plugins/timeago.js',
-		'@/plugins/toasts.client.js',
-		'@/plugins/torn.client.js',
-	],
-
-	// Auto import components: https://go.nuxtjs.dev/config-components
-	components: true,
-
-	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-	buildModules: [
-		// https://go.nuxtjs.dev/eslint
-		'@nuxtjs/eslint-module',
-	],
-
-	// Modules: https://go.nuxtjs.dev/config-modules
-	modules: [
-		// https://go.nuxtjs.dev/bootstrap
-		'bootstrap-vue/nuxt',
-		// https://go.nuxtjs.dev/axios
-		'@nuxtjs/axios',
-	],
-
-	publicRuntimeConfig: {
-		APP_VERSION: JSON.stringify(version),
-		BUILD_TIMESTAMP: new Date().getTime(),
-		CHANGELOG: JSON.stringify(changelog)
-	},
-
-	// Axios module configuration: https://go.nuxtjs.dev/config-axios
 	axios: {
-		baseURL: 'https://api.torn.com' // axios is the only API we use, in services/torn.js
+		baseURL: 'https://api.torn.com' // TORN is the only API we use, in services/torn.js
 	},
-
 	bootstrapVue: {
 		bootstrapCSS: false,
 		bootstrapVueCSS: false,
@@ -98,17 +33,61 @@ export default {
 			'BNavbar',
 			'BNavbarNav',
 			'BNavItem',
-		]
+		],
 	},
-
-	// Build Configuration: https://go.nuxtjs.dev/config-build
 	build: {
 		babel: {
 			compact: true,
 		},
 	},
-
+	buildModules: [
+		'@nuxtjs/eslint-module',
+	],
+	components: true,
+	css: [
+		'@/assets/style.scss'
+	],
+	head: {
+		title: 'TLL',
+		titleTemplate: '%s | TLL',
+		htmlAttrs: {
+			lang: 'en'
+		},
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: 'TLL is a tool which aims to help TORN City\'s loss sellers by providing them automatic counting, performance statistics and a proof system.' }
+		],
+		link: [
+			{
+				rel: 'stylesheet',
+				href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css'
+			},
+			{
+				rel: 'stylesheet',
+				href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/fontawesome.min.css'
+			}
+		],
+		// TODO favicon, fonts, rewrite description
+	},
 	loading: {
 		color: '#007bff', // $primary
-	}
+	},
+	modules: [
+		'bootstrap-vue/nuxt',
+		'@nuxtjs/axios',
+	],
+	plugins: [
+		'@/plugins/database.client.js',
+		'@/plugins/storage.client.js',
+		'@/plugins/timeago.js',
+		'@/plugins/toasts.client.js',
+		'@/plugins/torn.client.js',
+	],
+	publicRuntimeConfig: {
+		APP_VERSION: JSON.stringify(version),
+		BUILD_TIMESTAMP: new Date().getTime(),
+		CHANGELOG: JSON.stringify(changelog)
+	},
+	target: 'static',
 };
