3b12bf881e7e0f5997d59ca1b3a56993fab66f05
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-12-17 18:13:00 +0100

    fixed localstorage -> store import

--- store/index.js
+++ store/index.js
@@ -1,6 +1,11 @@
 export const mutations = {
 	INIT(state, newState) {
-		const mergedState = Object.assign(state, newState);
+		const mergedState = {};
+		Object.keys(state).forEach(k => {
+			// we have to merge in key level
+			// because if we merge at top level, new store fields get removed
+			mergedState[k] = { ...state[k], ...newState[k] };
+		});
 		this.replaceState(mergedState);
 	}
 };
