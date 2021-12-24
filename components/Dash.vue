26c947799db51c89f6baf55e9bfc931e7f556a18
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2021-12-24 10:04:06 +0100

    rebuilt chart, adding legends (not perfect atm)

--- components/Dash.vue
+++ components/Dash.vue
@@ -1,30 +1,40 @@
 <template>
-	<Card>
-		<div class="card-body d-flex">
-			<div class="d-flex flex-column w-100">
-				<h6>Last {{ items.length }} days</h6>
-				<div class="d-flex flex-column">
+	<div>
+		<Card>
+			<div class="card-body">
+				<h6 class="mb-4">Attacks in the past {{ items.length }} days</h6>
+				<div class="d-flex">
+					<div class="d-flex flex-column justify-content-between px-2 text-muted">
+						<div>{{ max }}</div>
+						<div>0</div>
+					</div>
 					<div
-						class="align-items-end d-flex flex-grow-1 flex-row-reverse position-relative"
-						style="min-height: 300px;"
+						class="align-items-end border-bottom border-left d-flex flex-grow-1 flex-row-reverse position-relative"
+						style="height: 300px;"
 					>
 						<div
-							class="avg bg-info position-absolute w-100"
+							class="avg border-bottom position-absolute px-1 text-info w-100"
 							:style="{ bottom: avg ? (100 * avg / max) + '%' : 0 }"
+							v-text="'Avg'"
 						/>
 						<div
 							v-for="(attacks, i) in items"
 							:key="days[i]"
-							v-b-tooltip.hover.bottom.html
-							class="bg-primary flex-grow-1 transitions"
+							v-b-tooltip.bottom.html
+							class="bar bg-primary flex-grow-1"
 							:style="{ height: 100 * attacks.length / max + '%' }"
 							:title="labels[i]"
 						/>
 					</div>
 				</div>
+				<div class="d-flex p-2 text-muted">
+					<div class="d-none d-sm-block mr-auto">{{ format(days[days.length - 1]) }}</div>
+					<div class="d-sm-none mx-auto">Days</div>
+					<div class="d-none d-sm-block ml-auto">{{ format(days[0]) }}</div>
+				</div>
 			</div>
-		</div>
-	</Card>
+		</Card>
+	</div>
 </template>
 
 <script>
@@ -43,7 +53,7 @@ export default {
 		...mapState('ui', ['loading']),
 		labels() {
 			return this.items.map((attacks, i) => {
-				const d = dayjs(this.days[i]).format('DD/MM/YY');
+				const d = this.format(this.days[i]);
 				return `<strong>${attacks.length}</strong> attack${
 					attacks.length === 1 ? '' : 's'
 				} on <strong>${d}</strong>`;
@@ -51,10 +61,11 @@ export default {
 		},
 		avg() {
 			if (this.items.length) {
-				const sum = this.items
-					.map(attacks => attacks.length)
-					.reduce((sum, len) => sum + len);
-				return sum / this.items.length;
+				const lens = this.items
+					.slice(1) // exclude today (incomplete)
+					.map(attacks => attacks.length);
+				const sum = lens.reduce((sum, len) => sum + len);
+				return sum / lens.length;
 			} else {
 				return 0;
 			}
@@ -84,6 +95,9 @@ export default {
 	},
 	methods: {
 		...mapMutations('ui', ['SET_LOADING']),
+		format(ts) {
+			return dayjs(ts).format('DD/MM/YY');
+		},
 		async query() {
 			this.SET_LOADING(true);
 			this.days = DB.pastDays(this.limit);
@@ -100,18 +114,22 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import 'bootstrap/scss/functions';
+@import 'bootstrap/scss/variables';
+@import 'bootstrap/scss/mixins';
+
 .avg {
-	height: 2px;
+	border-color: $info !important;
 	left: 0;
-	opacity: 0.3;
+	pointer-events: none;
 	transition: bottom 0.2s;
+	z-index: 1;
 }
 
 .bar {
 	margin: 0 2px;
 	transition: height 0.2s;
-	z-index: 1;
 }
 
 .bar:nth-child(2) {
